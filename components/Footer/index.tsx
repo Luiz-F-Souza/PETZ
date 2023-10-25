86ec71186bf510719afdd43a9b500898cd9c5f9d
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2023-10-25 17:01:31 -0300

    feat: create global footer

---
+++ components/Footer/index.tsx
@@ -0,0 +1,16 @@
+"use server"
+
+export const Footer = () => {
+
+  return (
+    <footer
+      className="
+        bg-dark 
+          flex items-center justify-center
+          py-8
+        "
+    >
+      <p className="text-white">Todas as marcas e ilustrações utilizadas são de seus resepctivos donos.</p>
+    </footer>
+  )
+}
