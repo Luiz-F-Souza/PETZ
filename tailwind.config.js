3b07e1880443fda37d850dbcc76dbf7f32f22859
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2023-10-25 16:52:49 -0300

    feat: added tailwind into project

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      borderWidth:{
+        1: '1px'
+      },
+      fontSize:{
+        'full': '2rem'
+      }
+    },
+    colors:{
+      primary: {
+        500: '#E40F0F'
+      },
+      gray: {
+        muted: '#747474',
+        300: '#D5D5D5',
+        700: '#1D1D1D'
+      },
+      white: '#fff',
+      "off-white": '#EEEEEE',
+      dark: '#1D1D1D'
+    }
+  },
+  plugins: [],
+}
+
