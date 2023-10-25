3f7cac9cd67b76ce4680ddd16e6d807fe4a05468
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2023-10-25 17:10:40 -0300

    feat: create global layout with header and footer

---
+++ components/Layout/index.tsx
@@ -0,0 +1,19 @@
+"use server"
+
+import { ReactNode } from "react"
+import { Header } from "../Header"
+import { Footer } from "../Footer"
+
+type LayoutProps = {
+  children: ReactNode
+}
+export const Layout = ({ children }: LayoutProps) => {
+
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  )
+}
